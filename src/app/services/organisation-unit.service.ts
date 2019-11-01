41fb3c1b35de9297f84456c9dbfeaec820fe7ec1
Bennett <bennybenester@gmail.com>
2019-11-01 16:04:03 +0300

    :sparkles: change api url for getting orgUnits

--- src/app/services/organisation-unit.service.ts
+++ src/app/services/organisation-unit.service.ts
@@ -1,16 +1,17 @@
-import { Injectable } from '@angular/core';
-import { NgxDhis2HttpClientService } from '@iapps/ngx-dhis2-http-client';
-import { Observable } from 'rxjs';
-import { OrganisationUnitChildren } from '../models/organisation-unit.model';
+import { Injectable } from "@angular/core";
+import { NgxDhis2HttpClientService } from "@iapps/ngx-dhis2-http-client";
+import { Observable } from "rxjs";
+import { OrganisationUnitChildren } from "../models/organisation-unit.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class OrganisationUnitService {
   constructor(private httpServuice: NgxDhis2HttpClientService) {}
   getOrgunitChildren(orgunitId: string): Observable<any> {
     const fields =
       'fields=name,children[id,lastUpdated,contactPerson,level,name,shortName,leaf,displayName,displayShortName,openingDate,parent,path,coordinates,children[id,name,level]]';
+      // "fields=id,name,lastUpdated,phoneNumber,level,attributeValues[value,attribute[id,name]]&filter=path:ilike:6";
     return this.httpServuice.get(
       `organisationUnits/${orgunitId}.json?fields=${fields}`
     );
