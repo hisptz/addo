b0867f534682ac341c023241ba0c928d93454482
Bennett <bennybenester@gmail.com>
2020-02-21 05:53:03 +0300

    refactor: change removed links imports

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -1,10 +1,8 @@
-import { HomeComponent } from './home/home.component';
 import { OrganisationUnitDetailsComponent } from './organisation-unit-details/organisation-unit-details.component';
 import { OrganisationUnitsComponent } from './organisation-units/organisation-units.component';
 import { OrganisationUnitEditComponent } from './organisation-unit-edit/organisation-unit-edit.component';
 
 export const pages: any[] = [
-  HomeComponent,
   OrganisationUnitEditComponent,
   OrganisationUnitDetailsComponent,
   OrganisationUnitsComponent,
