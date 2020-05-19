66bea9d7f04af1a51a0cfc18a7bdea0afc0b3e8e
Bennett <bennybenester@gmail.com>
2020-05-19 20:37:31 +0300

    test: Correct imports in test files

--- src/app/pages/organisation-unit-edit/organisation-unit-edit.component.spec.ts
+++ src/app/pages/organisation-unit-edit/organisation-unit-edit.component.spec.ts
@@ -5,17 +5,20 @@ import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from 'src/app/store/reducers';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ReactiveFormsModule } from '@angular/forms';
-import {
-  MatMenuModule,
-  MatButtonModule,
-  MatCardModule,
-  MatIconModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatDatepickerModule,
-  MatNativeDateModule,
-  MatButtonToggleModule
-} from '@angular/material';
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 describe('OrganisationUnitEditComponent', () => {
   let component: OrganisationUnitEditComponent;
