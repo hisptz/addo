5b2a50edcc609c6622840162be0ad8c0e1b49a02
Bennett <bennybenester@gmail.com>
2020-05-27 19:31:02 +0300

    style: Move material tabs to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,51 @@
-import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { Title } from '@angular/platform-browser';
+import { Component } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
+import { Title } from "@angular/platform-browser";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  navLinks: any[];
+  activeLinkIndex = -1;
   constructor(
     private translate: TranslateService,
-    private titleService: Title
+    private titleService: Title,
+    private router: Router
   ) {
     // this language will be used as a fallback when a translation isn't found in the current language
-    this.translate.setDefaultLang('en');
+    this.translate.setDefaultLang("en");
 
     // the lang to use, if the lang isn't available, it will use the current loader to get them
-    this.translate.use('en');
+    this.translate.use("en");
 
     // Set application title
-    this.setTitle('Addo & Labs');
+    this.setTitle("Addo & Labs");
+    this.navLinks = [
+      {
+        label: "Addos Reporting Status",
+        link: "./facilities",
+        index: 0,
+      },
+      {
+        label: "SMS Automation",
+        link: "./performancesms",
+        index: 1,
+      },
+    ];
   }
 
   public setTitle(newTitle: string) {
     this.titleService.setTitle(newTitle);
+  }
+  ngOnInit(): void {
+    this.router.events.subscribe((res) => {
+      this.activeLinkIndex = this.navLinks.indexOf(
+        this.navLinks.find((tab) => tab.link === "." + this.router.url)
+      );
+    });
   }
 }
