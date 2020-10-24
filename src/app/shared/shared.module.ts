20af2c6c1949ba68cb350542cd6f26d8f3f7c447
Bennett Benedict <bennybenester@gmail.com>
2020-10-24 22:13:13 +0300

    feat: add shared module imports

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { sharedComponents } from '.';
+import { materialModules } from './material.module';
 
 @NgModule({
-  imports: [CommonModule, FormsModule],
-  declarations: [],
+  imports: [CommonModule, FormsModule, ...materialModules],
+  exports: [...sharedComponents],
+  declarations: [...sharedComponents],
 })
 export class SharedModule {}
